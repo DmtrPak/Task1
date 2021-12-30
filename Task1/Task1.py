d05800e01206b509039117edfaaa0f242b3b200d
DmtrPak <96635321+DmtrPak@users.noreply.github.com>
2021-12-30 13:30:52 +0200

    Update Task1.py

--- Task1/Task1.py
+++ Task1/Task1.py
@@ -9,14 +9,7 @@ task1_1 = 0
 task1_2 = 0
 task1_3 = 0
 task1_4 = 0
-task1_5 = {
-            "854x480": 0,
-            "856x480": 0,
-            "960x540": 0,
-            "720x576": 0,
-            "1280x720": 0,
-            "1920x1080": 0
-    }
+task1_5 = {}
 for i in data["streams"]:
     if re.match("cam(.*)", i["name"]) is None:
         if i["name"] != "ohotnik_i_rybolov_319":
@@ -24,19 +17,12 @@ for i in data["streams"]:
                 check += 1
                 if i["media_info_json"]["tracks"][check]["content"] != "video":
                     check += 1
-            if (i["media_info_json"]["tracks"][check]["pixel_width"] == 854):
-                task1_5["854x480"] += 1   
-            if (i["media_info_json"]["tracks"][check]["pixel_width"] == 856):
-                task1_5["856x480"] += 1
-            if (i["media_info_json"]["tracks"][check]["pixel_width"] == 960):
-                task1_5["960x540"] += 1
-            if (i["media_info_json"]["tracks"][check]["pixel_width"] == 720):
-                task1_5["720x576"] += 1
-            if (i["media_info_json"]["tracks"][check]["pixel_width"] == 1280):
-                task1_5["1280x720"] += 1
-            if (i["media_info_json"]["tracks"][check]["pixel_width"] == 1920):
-                task1_5["1920x1080"] += 1
-            check = 0
+            conc = str(i["media_info_json"]["tracks"][check]["pixel_width"]) + "x" + str(i["media_info_json"]["tracks"][check]["pixel_height"])
+            if conc in task1_5:
+                task1_5[conc] += 1
+            else:
+                task1_5[conc] = 1
+        print(i["name"] + ": " + conc)  
 
 
         if re.match("...:\/\/239.100.1.6(.*)", i["url"]): #offline (239.100.1.6)
@@ -50,15 +36,9 @@ for i in data["streams"]:
                 else:
                     if re.match("...:\/\/238.166(.*)", i["url"]) is None and re.match("...:\/\/238(.*)", i["url"]):  #starting 238 with out 238.166
                         task1_4 += 1
-       # print(i["name"])
 
 print(str("Task 1: " + str(task1_1)))
 print(str("Task 2: " + str(task1_2)))
 print(str("Task 3: " + str(task1_3)))
 print(str("Task 4: " + str(task1_4)))
-print(str("854x480: " + str(task1_5["854x480"])))
-print(str("856x480: " + str(task1_5["856x480"])))
-print(str("960x540: " + str(task1_5["960x540"])))
-print(str("720x576: " + str(task1_5["720x576"])))
-print(str("1280x720: " + str(task1_5["1280x720"])))
-print(str("1920x1080: " + str(task1_5["1920x1080"])))
+print(str(task1_5))
